c40362fe9390046f7a2bd5a7b33b953e9de8f05d
Penumudi Datta Jagan Mohan <139993968+Dattajagan@users.noreply.github.com>
2025-05-29 21:59:10 +0530

    Update tailwind.config.js

--- app/data/tailwind.config.js
+++ app/data/tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class', // ✅ THIS IS CRUCIAL
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} 
+// module.exports = {
+//   darkMode: 'class', // ✅ THIS IS CRUCIAL
+//   content: [
+//     './app/**/*.{js,ts,jsx,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }; //
