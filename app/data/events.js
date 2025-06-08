a2ff0f0327f4115b67d1a27bf1ccc4b625b8670a
Penumudi Datta Jagan Mohan <139993968+Dattajagan@users.noreply.github.com>
2025-06-08 06:41:07 +0530

    Update events.js

--- app/data/events.js
+++ app/data/events.js
@@ -1,8 +1,18 @@
 export const events = [
   {
-    title: "Microsoft AI-DataFest",
+    title: "AI-DataFest - Microsoft",
     date: "03 May 2025",
     description: "Participated in the AI-DataFest event in the Microsoft office"
+  },
+  {
+    title: "Road Safety Hackathon - Unstop",
+    date: "April 2025",
+    description: "Participated in national hackathon building innovative road safety solutions."
+  },
+  {
+    title: "Treasure Hunt - Eurogrip(Unstop)",
+    date: "March 2025",
+    description: "Participated in a treasure hunt event organized by Eurogrip on Unstop platform."
   },
   {
     title: "Campus Ambassador - Wayspire",
@@ -13,10 +23,6 @@ export const events = [
     title: "AICTE Internship - Eduskills",
     date: "Jan - Mar 2024",
     description: "Worked on AWS and Cloud-based projects."
-  },
-  {
-    title: "Road Safety Hackathon - Unstop",
-    date: "2025",
-    description: "Participated in national hackathon building innovative road safety solutions."
   }
+  
 ];
